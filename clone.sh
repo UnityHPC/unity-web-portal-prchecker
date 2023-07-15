ea1738de75dc757ad063af676d977ade187308d8
Hakan Saplakoglu <hakansaplakog@gmail.com>
2023-07-15 16:07:07 -0400

    added setup lines

--- clone.sh
+++ clone.sh
@@ -7,4 +7,13 @@ clone_path=pr$pr_id
 
 git clone $git_url $clone_path
 
-cd $clone_path && gh pr checkout $pr_id
+cd $clone_path
+
+# Checkout PR
+gh pr checkout $pr_id
+
+# Initial Setup
+composer update
+cd tools/docker-dev
+
+./build.sh
